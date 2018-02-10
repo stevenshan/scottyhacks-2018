934fae68cb49e7acb9f10cb6d9a53cbc5e5979aa
Steven Shan <steven.shan99@gmail.com>
2018-02-10 17:20:01 -0500

    temp delete

--- assets/js/interactions.js
+++ assets/js/interactions.js
@@ -108,7 +108,7 @@ function add_class()
 
 function class_click_action(elem)
 {
-	if ($("#delete_toggle").prop("checked"))
+	if (delete_mode)
 	{
 		delete classes[$(elem).attr("id").slice(1)];	
 		update_graph();
