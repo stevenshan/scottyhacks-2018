9e30a5adc840d6335ec78fdf889602813c0255c2
Hannah Sun <hannah21sun@gmail.com>
2018-02-10 15:46:37 -0500

    First testing for hover, updated script for jquery in index.html

--- assets/js/tooltips.js
+++ assets/js/tooltips.js
@@ -1,4 +1,4 @@
-
+/*
 $(function() {
   $(document).tooltip({
     items: ".class",
@@ -8,32 +8,30 @@ $(function() {
       return courses_dict[cmu_class_id];
     }
   });
-});
-/*
+});*/
 
-var course_files = ["classes/fall-2017.json", "classes/spring-2018.json"];
 
 function hover() {
     $('.class').hover(function () {  
-        var target = '#' + ($(this).desc('data-popbox'));
+        var target = $(this).find(".class_id")[0].innerHTML;
         $(target).show();
         moveLeft = $(this).outerWidth();
         moveDown = ($(target).outerHeight() / 2);
     }, 
 
     function () {
-        var target = '#' + ($(this).attr('data-popbox'));
+        var target = $(this).find(".class_id")[0].innerHTML;
         if (!($(".class").hasClass("show"))) {
             $(target).hide(); //dont hide popup if it is clicked
         }   
     });
 
-
+/*
     $('.class').click(function (e) {
-        var target = '#' + ($(this).attr('data-popbox'));
+        var target = $(this).find(".class_id")[0].innerHTML;
         if (!($(this).hasClass("show"))) {
             $(target).show();
         }
     $(this).toggleClass("show");
-    }
-}*/
+    }*/
+}
