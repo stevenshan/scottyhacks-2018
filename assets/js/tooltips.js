795a8d8aeba2df5a893e4ca2a1b9d34c1eabf027
Hannah Sun <hannah21sun@gmail.com>
2018-02-10 17:46:36 -0500

    Updated hover function

--- assets/js/tooltips.js
+++ assets/js/tooltips.js
@@ -1,40 +1,10 @@
-/*
-$(function() {
-  $(document).tooltip({
-    items: ".class",
-    content: function() {
-      let cmu_class_id = $(this).find(".class_id")[0].innerHTML;
-      console.log(cmu_class_id);
-      return courses_dict[cmu_class_id];
-    }
-  });
-});*/
-
-
 $(document).ready(function(){
     $(".class").hover(function(){
-        let cmu_class_id = $(this).find(".class_id")[0].innerHTML;
+        let cmu_class_id = get_elem_id(this);
         console.log(cmu_class_id);
-        var target = courses_dict[cmu_class_id]; 
+        var target = courses_dict[cmu_class_id].desc; 
         $(target).show();
         moveLeft = $(this).outerWidth();
         moveDown = ($(target).outerHeight() / 2);
     });
 });
-
-/*
-function hover() {
-    $('.class').hover(function () {  
-        var target = $(this).find(".class_id")[0].innerHTML;
-        $(target).show();
-        moveLeft = $(this).outerWidth();
-        moveDown = ($(target).outerHeight() / 2);
-    }, 
-
-    function () {
-        var target = $(this).find(".class_id")[0].innerHTML;
-        if (!($(".class").hasClass("show"))) {
-            $(target).hide(); //dont hide popup if it is clicked
-        }   
-    });
-}
