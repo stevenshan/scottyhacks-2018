4e826df32f45524112e27b5634c1c6c9ffe42893
Hannah Sun <hannah21sun@gmail.com>
2018-02-10 04:55:09 -0500

    Fixed constructors in paper

--- assets/js/graph.js
+++ assets/js/graph.js
@@ -57,23 +57,23 @@ function connect_courses(elem1, elem2)
 }
 
 function makeRectangle(topLeft, size, cornerSize, colour){
-    var rectangle = new Rectangle(topLeft, size);
+    var rectangle = new paper.Rectangle(topLeft, size);
     var cornerSize = cornerSize;
-    var path = new Path.RoundRectangle(rectangle, cornerSize);
+    var path = new paper.Path.RoundRectangle(rectangle, cornerSize);
     path.fillColor = colour;
     return path;
 }
 
 function connectTest() {
-	var xy1 = new Point(50,50); //Position of 1st rectangle.
-    var size = new Size(100, 80); //Size
-    var c = new Size(8,8); //Corner radius
+	var xy1 = new paper.Point(50,50); //Position of 1st rectangle.
+    var size = new paper.Size(100, 80); //Size
+    var c = new paper.Size(8,8); //Corner radius
     var col = "#167ee5"; //Color
     
     var r1 = makeRectangle(xy1, size, c, col); //Make first rectangle
     
-    var xy2 = new Point(467,310); //Position of second rectangle
-    var size2 = new Size(115, 70); //Size of second rectangle
+    var xy2 = new paper.Point(467,310); //Position of second rectangle
+    var size2 = new paper.Size(115, 70); //Size of second rectangle
     
     var r2 = makeRectangle(xy2, size2, c, col); //Make secont rectangle
     
@@ -81,10 +81,10 @@ function connectTest() {
     
     var r2cent = r2.bounds.center;
     
-    var rc = new Rectangle(r1cent, r2cent);
+    var rc = new paper.Rectangle(r1cent, r2cent);
     
-    var c1 = new Path.Circle(rc.topCenter, 3);
-    var c2 = new Path.Circle(rc.bottomCenter, 3);
+    var c1 = new paper.Path.Circle(rc.topCenter, 3);
+    var c2 = new paper.Path.Circle(rc.bottomCenter, 3);
     c1.fillColor = c2.fillColor = 'red';
 
     // the handles are relative to the path's point
@@ -92,10 +92,10 @@ function connectTest() {
     h1 = rc.topCenter - r1cent;
     h2 = rc.bottomCenter - r2cent;
  
-    var r1seg = new Segment(r1cent, null, h1);
-    var r2seg = new Segment(r2cent, null, h2);
+    var r1seg = new paper.Segment(r1cent, null, h1);
+    var r2seg = new paper.Segment(r2cent, null, h2);
     
-    var connector = new Path(r1seg, r2seg); //Ok so I made this path... Now what? How do access and edit the handlers at endpoints like in the image?
+    var connector = new paper.Path(r1seg, r2seg); //Ok so I made this path... Now what? How do access and edit the handlers at endpoints like in the image?
     
     connector.strokeColor = 'black'; //Give it some colour so we can see it.
 
